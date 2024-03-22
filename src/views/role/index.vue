add01320cd5717552ecead3823ac4730f3b823e7
bubble486 <2766193137@qq.com>
2024-03-22 14:02:28 +0800

    完成表格列的自定义结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -9,9 +9,23 @@
       <el-table :data="list">
         <!-- 放置列 -->
         <el-table-column prop="name" label="角色" width="200px" align="center" />
-        <el-table-column prop="state" label="启用" width="200px" align="center" />
+        <el-table-column prop="state" label="启用" width="200px" align="center">
+          <!-- 自定义列结构 （插槽 -->
+          <!-- row 是组件可以接收到的数据 -->
+          <template v-slot="{row}">
+            <span>{{ row.state===1?"已启用": row.state===0 ? "未启用" : "无" }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="description" label="描述" align="center" />
-        <el-table-column label="操作" align="center" />
+        <el-table-column label="操作" align="center">
+          <!-- 为了后面方便拿到数据使用template标签包裹 -->
+          <template>
+            <!-- 放置操作按钮 -->
+            <el-button size="mini" type="text">分配权限</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 放置分页组件 -->
       <!-- el-row 是布局组件，可以设置相应的布局 -->
