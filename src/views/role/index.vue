b4d756274021443d0112be21d688d2d74ee072bd
bubble486 <2766193137@qq.com>
2024-03-22 14:39:11 +0800

    角色管理-添加角色的弹层页面结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -3,7 +3,7 @@
     <div class="app-container">
       <!-- 角色管理内容 -->
       <div class="role-operate">
-        <el-button size="mini" type="primary">添加角色</el-button>
+        <el-button size="mini" type="primary" @click="showDialog=true">添加角色</el-button>
       </div>
       <!-- 放置table组件 -->
       <el-table :data="list">
@@ -40,6 +40,30 @@
         />
       </el-row>
     </div>
+    <!-- 放置弹层 -->
+    <el-dialog title="新增角色" width="500px" :visible.sync="showDialog">
+      <!-- 表单内容 -->
+      <el-form label-width="120px">
+        <el-form-item label="角色名称">
+          <el-input style="width:300px" size="mini" />
+        </el-form-item>
+        <el-form-item label="启用">
+          <el-switch size="mini" />
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input type="textarea" :rows="3" style="width:300px" size="mini" />
+        </el-form-item>
+        <el-form-item>
+          <el-row type="flex" justify="center">
+            <el-col :span="12">
+              <el-button size="mini" type="primary">确认</el-button>
+              <el-button size="mini">取消</el-button>
+            </el-col>
+          </el-row>
+
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -50,6 +74,8 @@ export default {
   data() {
     return {
       list: [],
+      // 控制弹层显示隐藏
+      showDialog: false,
       // 将分页信息，放到对象中
       pageParams: {
         page: 1, // 第几页
