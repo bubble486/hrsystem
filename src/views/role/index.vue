05cc4447013c148ecdd07a41be165efbcf32222a
bubble486 <2766193137@qq.com>
2024-03-23 22:00:55 +0800

    权限分配-给角色分配权限（弹出层

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -42,7 +42,8 @@
             <template v-else>
               <!-- 非编辑状态 -->
               <!-- 放置操作按钮 -->
-              <el-button size="mini" type="text">分配权限</el-button>
+              <!-- 点击按钮时候记录 当前id的值 -->
+              <el-button size="mini" type="text" @click="btnPermission(row.id)">分配权限</el-button>
               <el-button size="mini" type="text" @click="btnEditRow(row)">编辑</el-button>
               <!-- 使用气泡确认框包裹按钮 -->
               <el-popconfirm title="您确定要删除该管理员？" @onConfirm="confirmDelete(row.id)">
@@ -67,7 +68,7 @@
         />
       </el-row>
     </div>
-    <!-- 放置弹层 -->
+    <!-- 放置添加角色的弹层 -->
     <!-- 使用x按钮关闭表单时候不会 重置表单 监听close属性，触发btnCancel方法 -->
     <el-dialog title="新增角色" width="500px" :visible.sync="showDialog" @close="btnCancel">
       <!-- 表单内容 -->
@@ -94,10 +95,22 @@
         </el-form-item>
       </el-form>
     </el-dialog>
+    <!-- 放置权限弹层 -->
+    <el-dialog :visible.sync="showPermissionDialog" title="分配权限">
+      <!-- 放置权限数据 -->
+      <el-tree
+        :data="permissionData"
+        :props="{ label:'name', children: 'children'}"
+        show-checkbox
+        default-expand-all
+      />
+    </el-dialog>
   </div>
 </template>
 <script>
 import { getRoleList, addRole, updateRole, deleteRole } from '@/api/role'
+import { getPermissionList } from '@/api/permission'
+import { transListToTreeData } from '@/utils'
 
 export default {
   name: 'Role',
@@ -121,7 +134,11 @@ export default {
       roleRules: {
         name: [{ required: true, message: '角色名称不能为空', tigger: 'blur' }],
         description: [{ required: true, message: '角色描述不能为空', tigger: 'blur' }]
-      }
+      },
+      showPermissionDialog: false,
+      permissionData: [],
+      // 记录当前选中的id
+      currentId: null
     }
   },
   created() {
@@ -200,6 +217,11 @@ export default {
       }
       this.getRoleList()
       // 如果还是拿出的不是最后一个，更新当前页面的数据
+    },
+    async btnPermission(id) {
+      this.showPermissionDialog = true
+      this.permissionData = transListToTreeData(await getPermissionList(), 0)
+      this.currentId = id
     }
   }
 }
