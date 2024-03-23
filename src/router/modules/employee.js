ca62668cf6025ca796c89cd495ed6c2dc4c0d5d9
bubble486 <2766193137@qq.com>
2024-03-23 13:19:20 +0800

    员工详情-实现了员工详情的结构和路由配置

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -10,5 +10,16 @@ export default {
       title: '员工',
       icon: 'people'
     }
+  }, {
+    // 员工详情页
+    path: '/employee/detail',
+    component: () => import('@/views/employee/detail.vue'),
+    hidden: true,
+    meta: {
+      // 显示在导航栏
+      title: '员工详情'
+    }
   }]
 }
+
+// 只想在左侧菜单显示一级菜单的话 让二级路由只有一个显示在左侧菜单
