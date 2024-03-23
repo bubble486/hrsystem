242140f9e2792fb078d83929afd48c0c5c202b27
bubble486 <2766193137@qq.com>
2024-03-23 20:31:04 +0800

    权限管理-页面权限数据的获取和显示

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -2,13 +2,14 @@
   <div class="container">
     <div class="app-container">
       <el-button class="btn-add" size="mini" type="primary">添加权限</el-button>
-      <el-table>
-        <el-table-column label="名称" />
-        <el-table-column label="标识" />
-        <el-table-column label="描述" />
+      <el-table :data="list" row-key="id" default-expand-all="">
+        <el-table-column prop="name" label="名称" />
+        <el-table-column prop="code" label="标识" />
+        <el-table-column prop="description" label="描述" />
         <el-table-column label="操作">
-          <template>
-            <el-button size="mini" type="text">添加</el-button>
+          <!-- 表格里面的数据要在插槽中取 -->
+          <template v-slot="{row}">
+            <el-button v-if="row.type===1" size="mini" type="text">添加</el-button>
             <el-button size="mini" type="text">编辑</el-button>
             <el-button size="mini" type="text">删除</el-button>
           </template>
@@ -18,8 +19,24 @@
   </div>
 </template>
 <script>
+import { getPermissionList } from '@/api/permission'
+import { transListToTreeData } from '@/utils'
+
 export default {
-  name: 'Permission'
+  name: 'Permission',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      this.list = transListToTreeData(await getPermissionList(), 0)
+    }
+  }
 }
 </script>
 
