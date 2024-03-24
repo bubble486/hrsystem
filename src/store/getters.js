779ff5b96defb8f514da100ea280cf29a9fa0c81
bubble486 <2766193137@qq.com>
2024-03-24 22:58:02 +0800

    首页-首页个人信息的展示

--- src/store/getters.js
+++ src/store/getters.js
@@ -8,6 +8,8 @@ const getters = {
   userId: state => state.user.userInfo.userId,
   avatar: state => state.user.userInfo.staffPhoto, // 头像
   name: state => state.user.userInfo.username, // 用户名
-  routes: state => state.user.routes // 路由信息
+  routes: state => state.user.routes, // 路由信息
+  company: state => state.user.userInfo.company, // 公司名称
+  departmentName: state => state.user.userInfo.departmentName // 部门名称
 }
 export default getters
